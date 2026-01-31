41605cfce317cda4fa5cdf660e5ceae4ced44e48
swaralijoglekar-10 <swaralij2001@gmail.com>
2026-01-31 22:13:02 +0530

    Second Update

--- src/lecture21to30/threads/ThreadSync.java
+++ src/lecture21to30/threads/ThreadSync.java
@@ -2,13 +2,17 @@ package lecture21to30.threads;
 
 public class ThreadSync extends Thread{
     public void run(){
-        System.out.println("Not synced");
+        System.out.println("Not synced"); // this statement can be accessed by multiple threads simultaneously
 
         synchronized (ThreadSync.class){ // only this block will be synced
             for(int i=1; i<=10; i++){
                 System.out.println(i);
             }
         }
+
+        //synchronized block ensures that only one thread at a time can execute the block synchronized on ThreadSync.class.
+        // Non-synchronized statements (like System.out.println("Not synced");) can still be executed by other threads concurrently
+        
     }
 }
 
